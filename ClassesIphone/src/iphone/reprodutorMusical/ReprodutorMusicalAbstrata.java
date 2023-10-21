c0bfaffff236e4fdc38e01ddb718eaf94d59680c
Clarice Monnerat <138911565+claricemonnerat@users.noreply.github.com>
2023-10-21 19:03:04 -0300

    Create ReprodutorMusicalAbstrata.java

---
+++ ClassesIphone/src/iphone/reprodutorMusical/ReprodutorMusicalAbstrata.java
@@ -0,0 +1,30 @@
+package com.dio.iphone.reprodutormusical;
+
+public abstract class ReprodutorMusicalAbstrata implements ReprodutorMusicalInterface {
+
+  protected String nome;
+
+  public ReprodutorMusicalAbstrata(String nome) {
+    this.nome = nome;
+  }
+
+  @Override
+  public void tocar() {
+    System.out.println(nome + " está tocando");
+  }
+
+  @Override
+  public void pausar() {
+    System.out.println(nome + " pausada!");
+  }
+
+  @Override
+  public void selecionarMusica(String nome) {
+    this.nome = nome;
+    System.out.println(nome + " selecionada");
+  }
+
+  public String getNome() {
+    return nome;
+  }
+}
