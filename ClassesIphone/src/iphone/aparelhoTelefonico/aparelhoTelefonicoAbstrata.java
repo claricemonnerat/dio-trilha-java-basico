d266381f20586b2e180bd57b9a826739343b6da2
Clarice Monnerat <138911565+claricemonnerat@users.noreply.github.com>
2023-10-21 19:14:16 -0300

    Create aparelhoTelefonicoAbstrata.java

---
+++ ClassesIphone/src/iphone/aparelhoTelefonico/aparelhoTelefonicoAbstrata.java
@@ -0,0 +1,19 @@
+package com.dio.iphone.aparelhotelefonico;
+
+public abstract class aparelhoTelefonicoAbstrata implements aparelhoTelefonicoInterface {
+
+  @Override
+  public void ligar(int num) {
+    System.out.println("Ligando para " + num);
+  }
+
+  @Override
+  public void atender() {
+    System.out.println("Atendendo a ligação");
+  }
+
+  @Override
+  public void iniciarCorreioVoz() {
+    System.out.println("Iniciando correio de voz");
+  }
+}
