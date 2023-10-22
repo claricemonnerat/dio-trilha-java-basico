f955d44d77720b66486674996d95c0478c18dc37
Clarice Monnerat <138911565+claricemonnerat@users.noreply.github.com>
2023-10-21 23:22:56 -0300

    Create ComportamentoNormal.java

---
+++ Padroes/src/ComportamentoNormal.java
@@ -0,0 +1,11 @@
+package com.dio.desafio;
+
+public class ComportamentoNormal implements Comportamento{
+
+	@Override
+	public void mover() {
+		System.out.println("Movendo-se normalmente...");
+		
+	}
+
+}
